0012661ead1adefd7be59f5c6f06e5ab697b1c4a
Moises Apaza <dotty@live.com.ar>
2018-11-06 16:57:31 -0300

    agregado insert para afiliados

--- resource/db_finan.sql
+++ resource/db_finan.sql
@@ -1,9 +1,14 @@
 create database db_finan;
 use db_finan;
 
+/* Insertar a tabla financiera */
 insert into financiera (nombre) values ('Naranja');
 insert into financiera (nombre) values ('Santander Rio');
 insert into financiera (nombre) values ('Galicia');
 
-
 select * from financiera;
+
+/* Insertar a la tabla afiliado */
+insert into afiliado (nombre,apellido,puesto,antiguedad,sueldo) values ('Martin','Ale','','3',20000);
+insert into afiliado (nombre,apellido,puesto,antiguedad,sueldo) values ('Daniel','Ramoz','','2',25000);
+insert into afiliado (nombre,apellido,puesto,antiguedad,sueldo) values ('Alex','Diaz','','5',30000);
