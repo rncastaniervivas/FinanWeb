ea4fc6fc903ff437989f65a1a9d9e69c3654059e
DavidBarrionuevo <david.barrionuevo96@hotmail.com>
2018-12-02 14:17:46 -0300

    faltaba el servicioAfiliadoMock.guardarAfiliado(afiliadoMock);

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/PrestamoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/PrestamoTest.java
@@ -12,6 +12,9 @@ import org.hibernate.SessionFactory;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,20 +34,36 @@ public class PrestamoTest {
 		ControladorPrestamo controlador = new ControladorPrestamo();
 		
 		ServicioPrestamo servicioPrestamoMock=mock(ServicioPrestamo.class);
+		ServicioAfiliado servicioAfiliadoMock=mock(ServicioAfiliado.class);
 		
 		Afiliado afiliadoMock=mock(Afiliado.class);
-		
+//		Prestamo prestamoMock=mock(Prestamo.class);
+//		prestamoMock.setAfiliado(afiliadoMock);
+//		prestamoMock.setCuotas(6);
+//		prestamoMock.setDni(9L);
+//		prestamoMock.setEstado("activo");
+//		prestamoMock.setInteres(2.00);
+//		prestamoMock.setValor(200);
+//		prestamoMock.setCuota(null);
+//		List<Prestamo> prestamos=new ArrayList<Prestamo>();		
+//		prestamos.add(prestamoMock);
 		afiliadoMock.setApellido("ApellidoMock");
 		afiliadoMock.setAntiguedad("10años");
 		afiliadoMock.setDni(9L);
 		afiliadoMock.setSueldo(10000.00);
 		afiliadoMock.setClasificacion("cliente");
 		afiliadoMock.setNombre("NombreMock");
-		afiliadoMock.setIdAfiliado(1L);
-
+//		afiliadoMock.setPrestamo(prestamos);
+//		afiliadoMock.setIdAfiliado(1L);
+		
+//		ModelAndView modelo= controlador.irValidarNuevoPrestamo(afiliadoMock, 500,6);
+		
+		servicioAfiliadoMock.guardarAfiliado(afiliadoMock);
+		
+		when(servicioAfiliadoMock.consultarAfiliadoDni(afiliadoMock.getDni())).thenReturn(afiliadoMock);
 		when(servicioPrestamoMock.prestamoDisponible(afiliadoMock)).thenReturn(3000.00);
 		
-		ModelAndView modelo= controlador.irValidarNuevoPrestamo(afiliadoMock, 500,6);
+		ModelAndView modelo=controlador.irANuevoPrestamo(afiliadoMock);
 		
 		assertThat(modelo.getViewName()).isEqualTo("nuevoprestamo");
 		
