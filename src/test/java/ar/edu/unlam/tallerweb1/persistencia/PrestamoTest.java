6020397dd16619c3a502abad185f3fceaa6f9a4d
DavidBarrionuevo <david.barrionuevo96@hotmail.com>
2018-12-02 15:50:22 -0300

    testPruebaQueSeValidoUnPrestamo

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/PrestamoTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/PrestamoTest.java
@@ -71,6 +71,60 @@ public class PrestamoTest {
 		
 		
 	}
+	@Test
+	@Transactional
+	@Rollback (true)
+	public void testPruebaQueSeValidoUnPrestamo() {
+		
+		ControladorPrestamo controlador = new ControladorPrestamo();
+		
+		ServicioPrestamo servicioPrestamoMock=mock(ServicioPrestamo.class);
+		ServicioAfiliado servicioAfiliadoMock=mock(ServicioAfiliado.class);
+		
+		controlador.setServicioAfiliado(servicioAfiliadoMock);
+		controlador.setServicioPrestamo(servicioPrestamoMock);
+		
+		Afiliado afiliadoMock=mock(Afiliado.class);
+		
+		Prestamo prestamoMock=mock(Prestamo.class);
+		
+		prestamoMock.setAfiliado(afiliadoMock);
+		prestamoMock.setCuotas(6);
+		prestamoMock.setDni(9L);
+		prestamoMock.setEstado("activo");
+		prestamoMock.setInteres(2.00);
+		prestamoMock.setValor(200);
+		prestamoMock.setCuota(null);
+		
+		List<Prestamo> prestamos=new ArrayList<Prestamo>();
+		
+		prestamos.add(prestamoMock);
+		afiliadoMock.setApellido("ApellidoMock");
+		afiliadoMock.setAntiguedad("10años");
+		afiliadoMock.setDni(9L);
+		afiliadoMock.setSueldo(10000.00);
+		afiliadoMock.setClasificacion("cliente");
+		afiliadoMock.setNombre("NombreMock");
+		afiliadoMock.setPrestamo(prestamos);
+//		afiliadoMock.setIdAfiliado(1L);
+		
+		
+		servicioAfiliadoMock.guardarAfiliado(afiliadoMock);
+		
+		when(servicioAfiliadoMock.consultarAfiliadoDni(afiliadoMock.getDni())).thenReturn(afiliadoMock);
+		
+		when(servicioPrestamoMock.consultarPrestamo(afiliadoMock.getDni())).thenReturn(prestamos);
+		
+		when(servicioPrestamoMock.prestamoDisponible(afiliadoMock)).thenReturn(3000.00);
+		
+		ModelAndView modelo= controlador.irValidarNuevoPrestamo(afiliadoMock, 500,6);
+		
+//		ModelAndView modelo=controlador.irANuevoPrestamo(afiliadoMock);
+		
+		assertThat(modelo.getViewName()).isEqualTo("/listarprestamos");
+		
+		
+	}
 
 }
 
